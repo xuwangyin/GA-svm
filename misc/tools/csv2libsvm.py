3f80c21f69bde5bae623a38ca5e1720887553280
xuwang <xuwangyin@gmail.com>
2012-04-14 22:55:32 +0800

    normalize regression data

--- misc/tools/csv2libsvm.py
+++ misc/tools/csv2libsvm.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import csv
-csv_reader=csv.reader(open('test.csv', 'rb'), delimiter=',')
+csv_reader=csv.reader(open('train.csv', 'rb'), delimiter=',')
 
-f = open('test.txt', 'w')
+f = open('train.txt', 'w')
+fixed_width = 20
 
 for row in csv_reader:
     s = ''
     for index, value in enumerate(row):
-        s = s + str(value) + ' ' if index == 0 else s + str(index) + ':' + str(value) + ' '
+        value_str =  str(float(value) / 20000.) + ' ' if index == 0 else str(index) + ':' + str(value) + ' '
+        if len(value_str) < fixed_width:
+            value_str += ' ' * (fixed_width - len(value_str))
+        s += value_str
     s += '\n'
     f.write(s)
 f.close()
